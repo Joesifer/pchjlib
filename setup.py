3c29e3cac80f77c05c2bd0f3f1cb5e262e5007a4
Joesifer <153426010+Joesifer@users.noreply.github.com>
2024-02-19 18:26:46 +0700

    info

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ desc = "{}\n\n{}".format(
 
 setup(
     name="pchjlib",
-    version="0.0.2.7",
+    version="0.0.2.8",
     author="Joesifer",
     description="Bộ sưu tầm hàm con của mình",
     long_description=desc,
